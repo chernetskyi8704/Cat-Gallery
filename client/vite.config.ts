86528e25b8784db99422910812d689e03e0b8f0b
Dmytro Chernetskyi <78321800+chernetskyi8704@users.noreply.github.com>
2024-11-03 23:14:36 +0200

    fix: update vite.config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -11,7 +11,9 @@ export default defineConfig({
     },
   },
   define: {
-    "process.env.VITE_BASE_API_URL": process.env.VITE_BASE_API_URL,
-    "process.env.VITE_API_KEY": process.env.VITE_API_KEY,
+    "process.env.VITE_BASE_API_URL": JSON.stringify(
+      process.env.VITE_BASE_API_URL
+    ),
+    "process.env.VITE_API_KEY": JSON.stringify(process.env.VITE_API_KEY),
   },
 });
